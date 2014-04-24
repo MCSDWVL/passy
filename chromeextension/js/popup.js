4d185c34231c98803276de27ea3f616d5a859327
Matthew Vassilakos <matthew.vassilakos@gmail.com>
2014-04-24 15:11:02 -0700

    copy to clipboard button

--- chromeextension/js/popup.js
+++ chromeextension/js/popup.js
@@ -5,18 +5,24 @@ document.addEventListener('DOMContentLoaded', function ()
 	document.getElementById('first-button').addEventListener('click', firstbuttonpressed);
 	document.getElementById('all-button').addEventListener('click', allbuttonpressed);
 
+	document.getElementById('clip-button').addEventListener('click', copybuttonpressed);
+
 	// add the field change listeners
 	document.getElementById('siteselect').addEventListener('input', inputchanged, false);
 	document.getElementById('masterpass').addEventListener('input', inputchanged, false);
 
 	// auto fill the site select box to the current url
 	var siteSelectBox = document.querySelector(".siteselect");
-	chrome.tabs.query({ currentWindow: true, active: true }, function (tabs)
+
+	if (chrome.tabs)
 	{
-		var tabURI = tabs[0].url;
-		var domain = tabURI.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
-		siteSelectBox.value = domain;
-	});
+		chrome.tabs.query({ currentWindow: true, active: true }, function (tabs)
+		{
+			var tabURI = tabs[0].url;
+			var domain = tabURI.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
+			siteSelectBox.value = domain;
+		});
+	}
 });
 
 function inputchanged(e)
@@ -28,26 +34,35 @@ function inputchanged(e)
 
 function fillActivePasswordField()
 {
-	chrome.tabs.executeScript({
-		code: 'if(document.activeElement && document.activeElement.type.toLowerCase() === "password") { document.activeElement.value = "' + document.getElementById('outputbox').value + '"; }'
-		//code: 'document.activeElement.value ="' + document.getElementById('outputbox').value + '";'
-	});
+	if (chrome.tabs)
+	{
+		chrome.tabs.executeScript({
+			code: 'if(document.activeElement && document.activeElement.type.toLowerCase() === "password") { document.activeElement.value = "' + document.getElementById('outputbox').value + '"; }'
+			//code: 'document.activeElement.value ="' + document.getElementById('outputbox').value + '";'
+		});
+	}
 };
 
 function fillFirstPasswordField()
 {
-	chrome.tabs.executeScript({
-		code: 'var ary = []; var inputs = document.getElementsByTagName("input"); for (var i=0; i<inputs.length; i++) { if (inputs[i].type.toLowerCase() === "password") { ary.push(inputs[i]); } }; if(ary.length > 0) { ary[0].value = "' + document.getElementById('outputbox').value + '"; }'
-		//code: 'document.activeElement.value ="' + document.getElementById('outputbox').value + '";'
-	});
+	if (chrome.tabs)
+	{
+		chrome.tabs.executeScript({
+			code: 'var ary = []; var inputs = document.getElementsByTagName("input"); for (var i=0; i<inputs.length; i++) { if (inputs[i].type.toLowerCase() === "password") { ary.push(inputs[i]); } }; if(ary.length > 0) { ary[0].value = "' + document.getElementById('outputbox').value + '"; }'
+			//code: 'document.activeElement.value ="' + document.getElementById('outputbox').value + '";'
+		});
+	}
 };
 
 function fillAllPasswordFields()
 {
-	chrome.tabs.executeScript({
-		code: 'var ary = []; var inputs = document.getElementsByTagName("input"); for (var i=0; i<inputs.length; i++) { if (inputs[i].type.toLowerCase() === "password") { ary.push(inputs[i]); } }; for(var i = 0; i<ary.length; ++i) { ary[i].value = "' + document.getElementById('outputbox').value + '"; }'
-		//code: 'document.activeElement.value ="' + document.getElementById('outputbox').value + '";'
-	});
+	if (chrome.tabs)
+	{
+		chrome.tabs.executeScript({
+			code: 'var ary = []; var inputs = document.getElementsByTagName("input"); for (var i=0; i<inputs.length; i++) { if (inputs[i].type.toLowerCase() === "password") { ary.push(inputs[i]); } }; for(var i = 0; i<ary.length; ++i) { ary[i].value = "' + document.getElementById('outputbox').value + '"; }'
+			//code: 'document.activeElement.value ="' + document.getElementById('outputbox').value + '";'
+		});
+	}
 };
 
 function firstbuttonpressed(e)
@@ -58,5 +73,12 @@ function firstbuttonpressed(e)
 function allbuttonpressed(e)
 {
 	fillAllPasswordFields();
+};
+
+function copybuttonpressed(e)
+{
+	document.getElementById("outputbox").select();
+	console.log(document.getElementById("outputbox").value);
+	document.execCommand("copy", false, null);
 };
 
